4f243221af07ce1ab5db5423905cb3599e61b8cc
Zohaib Khan <dev-zohaib@proton.me>
2026-02-24 18:21:11 +0500

    feat: update ScrapePage component to display active scrappers section with appropriate messaging

--- apps/client/src/pages/Scrape.tsx
+++ apps/client/src/pages/Scrape.tsx
@@ -1,3 +1,17 @@
 export default function ScrapePage() {
-    return <div>Scrape</div>;
+    return (
+        <div className="container p-4">
+            <div></div>
+            <div>
+                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                    Active scrappers
+                </h4>
+                <div>
+                    <p className="text-muted-foreground text-sm">
+                        No active scrappers found
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
